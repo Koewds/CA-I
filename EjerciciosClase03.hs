b9bcafee1b9bb8c12f6074a2d30512bb46ec2811
Koew <89328393+Koewds@users.noreply.github.com>
2021-09-05 21:36:46 -0300

    Add files via upload

---
+++ EjerciciosClase03.hs
@@ -0,0 +1,43 @@
+-- Dado un número natural, determinar si es múltiplo de 3 (con recursión)
+multiplo3 :: Int -> Bool
+multiplo3 n | n < 3 = False
+            | n == 3 = True
+            | otherwise = multiplo3 (n - 3)
+
+
+--Dado n perteneciente a N, sumar los primeros n números impares.
+sumaImpares :: Int -> Int
+sumaImpares n | n == 1 = 4
+              | n == 3 = 9
+              | n == 0 = 0
+              | mod n 2 == 0 = 4 + sumaImpares (n - 2)
+              | otherwise = 6 + sumaImpares (n - 2)
+--(En los impares, como el patrón se empieza a dar en n=3, resultan 3 casos base para que n=1 esté incluido)
+
+
+medioFact :: Int -> Int
+medioFact n | n == 1 = 1
+            | n == 2 = 2
+            | otherwise = n * medioFact (n - 2)
+
+--Suma de dígitos
+sumDigits :: Int -> Int
+sumDigits n | n == 0 = 0
+            | otherwise = sumDigits (div n 10) + mod n 10
+
+
+
+
+--Determinar si todos los dígitos de un número son iguales.
+equalDigits :: Int -> Bool
+equalDigits n |
+               |
+
+
+
+
+
+--Determinar si todos los dígitos de un número son iguales. (skip)
+equalDigits' :: Int -> Bool
+equalDigits' n | mod n 10 == mod (div n 10) 10 = True
+              | otherwise = False
